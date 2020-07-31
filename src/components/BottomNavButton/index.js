129939e79047c6c00cfc7a719e5b9a9550b4283e
Bishalsarang <bishalsarang@gmail.com>
2020-07-31 08:06:27 +0545

    Fix change priority bug

--- src/components/BottomNavButton/index.js
+++ src/components/BottomNavButton/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import './style.css';
+
 const BottomNavButton = ({ handleClick, visibility, icon }) => {
   return (
     <button
